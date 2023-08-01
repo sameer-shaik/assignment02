ccf589fc63b3c646e8612ca4cd2ea7356d003326
shaiksameer <shaiksameer911@gmail.com>
2023-08-01 19:05:28 +0530

    updated the code with backend feature

--- folder/environments/dev/access/main.tf
+++ folder/environments/dev/access/main.tf
@@ -1,31 +1,31 @@
-provider "aws" {
-    profile = var.account_map["access"]
-    alias = "access_provider"
-}
-
-provider "aws" {
-    profile = var.account_map["processing"]
-    alias = "process_provider"
-}
-
-variable "bucket_name" {}
-
-# UNBLOCK code for 1st version *******************************
 # provider "aws" {
-#     region                  = var.aws_accounts[var.acc_name].region
-#     access_key              = var.aws_accounts[var.acc_name].access_key
-#     secret_key              = var.aws_accounts[var.acc_name].secret_key
+#     profile = var.account_map["access"]
 #     alias = "access_provider"
 # }
 
 # provider "aws" {
-#     region                  = var.aws_accounts[var.acc_name_2].region
-#     access_key              = var.aws_accounts[var.acc_name_2].access_key
-#     secret_key              = var.aws_accounts[var.acc_name_2].secret_key
+#     profile = var.account_map["processing"]
 #     alias = "process_provider"
-# #   shared_credentials_file = "~/.aws/credentials"
-
 # }
+
+variable "bucket_name" {}
+
+# UNBLOCK code for 1st version *******************************
+provider "aws" {
+    region                  = var.aws_accounts[var.acc_name].region
+    access_key              = var.aws_accounts[var.acc_name].access_key
+    secret_key              = var.aws_accounts[var.acc_name].secret_key
+    alias = "process_provider"
+}
+
+provider "aws" {
+    region                  = var.aws_accounts[var.acc_name_2].region
+    access_key              = var.aws_accounts[var.acc_name_2].access_key
+    secret_key              = var.aws_accounts[var.acc_name_2].secret_key
+    alias = "access_provider"
+#   shared_credentials_file = "~/.aws/credentials"
+
+}
 
 # *************************************************
 
@@ -44,9 +44,9 @@ module "glue_database"{
 module "s3_bucket" {
 
     source = "../../../modules/s3_bucket"
-    var.bucket_name = join("-", [element(split("/", path.cwd), length(split("/", path.cwd))-1), "sss4one", 
+    # var.bucket_name = 
+    bucket = join("-", [element(split("/", path.cwd), length(split("/", path.cwd))-1), "sss4one", 
     element(split("/", path.cwd), length(split("/", path.cwd))-2)])
-    bucket = var.bucket_name
     providers = {
         aws = aws.process_provider
     }
--- folder/environments/dev/access/versions.tf
+++ folder/environments/dev/access/versions.tf
@@ -1,16 +1,16 @@
-# terraform {
-#     required_version = "~> 1.5.3"
-#     required_providers {
-#         aws = {
-#             source = "hashicorp/aws"
-#             version = "~> 5.9.0"
-#         }
-#     }
-#     # backend "s3" {
-#     #     bucket = "backendstatefilehandlerbucket"
-#     #     key = "state/terraform.tfstate"
-#     #     region = "us-east-1"
-#     #     dynamodb_table = "backend_ddb"
-#     # }
-# }
+terraform {
+    required_version = "~> 1.5.3"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.9.0"
+        }
+    }
+    backend "s3" {
+        bucket = "backendstatefilehandlerbucket"
+        key = "state/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "backend_ddb"
+    }
+}
 
