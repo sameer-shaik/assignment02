09ec63526db63f31bb353fb084ce4a9c6c248191
sameer-shaik <41112479+sameer-shaik@users.noreply.github.com>
2023-08-01 15:32:34 +0530

    removed var from bucket name

--- folder/environments/dev/access/main.tf
+++ folder/environments/dev/access/main.tf
@@ -44,7 +44,7 @@ module "glue_database"{
 module "s3_bucket" {
 
     source = "../../../modules/s3_bucket"
-    var.bucket_name = join("-", [element(split("/", path.cwd), length(split("/", path.cwd))-1), "sss4one", 
+    bucket_name = join("-", [element(split("/", path.cwd), length(split("/", path.cwd))-1), "sss4one", 
     element(split("/", path.cwd), length(split("/", path.cwd))-2)])
     bucket = var.bucket_name
     providers = {
